8d98d865558ecd2a697f535488a2bbb0b70018b0
aryamangithub <singharyaman357@gmail.com>
2024-12-22 21:11:52 +0530

    feat:user authentication check

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -100,7 +100,16 @@ export const updateProfile = async (req, res) => {
 
     } catch(error) {
         console.log("error in update profile:", error);
-        res.status(400).json({message: "Internal server error"})
+        res.status(500).json({message: "Internal server error"})
         
+    }
+}
+
+export const checkAuth = (req, res) => {
+    try{
+        res.status(200).json(req.user)  
+    } catch(error){
+        console.log("Error in checkAuth controller", error.message);
+        res.status(500).json({ message: "Internal Serve Error" })
     }
 }
